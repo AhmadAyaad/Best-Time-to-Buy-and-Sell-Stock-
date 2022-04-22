34ddcc92902893ad72e1c2990efcd3f1f1b630b4
Ahmad Ayaad <ahmed.ayyad@inetum.com>
2022-04-22 14:41:07 +0200

    Initial

---
+++ Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Best_Time_to_Buy_and_Sell_Stock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static int GetBestTimeToBuyAndSellInStock(int[] prices)
+        {
+            var minprice = Int32.MaxValue;
+            int maxprofit = 0;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (prices[i] < minprice)
+                    minprice = prices[i];
+                else if (prices[i] - minprice > maxprofit)
+                    maxprofit = prices[i] - minprice;
+            }
+            return maxprofit;
+        }
+    }
+}
